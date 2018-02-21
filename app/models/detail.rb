69ac3c43d62bc38e46b17c8f796ef05e708ca480
Natan667 <snakesnv@gmail.com>
2018-02-21 21:10:55 +0200

    Orders and precompile

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -2,7 +2,6 @@ class Detail < ApplicationRecord
 	belongs_to :author, class_name: "User"
 	has_many :details_orders
 	has_many :orders, :through => :details_orders, dependent: :destroy
-	accepts_nested_attributes_for :orders, reject_if: proc { |attributes| attributes[:name].blank? }, allow_destroy: true
 
 	def self.search(search)
     	where("lower(title) LIKE ?", "%#{search}%")
